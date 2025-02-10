2095a5c9f4542757a5662b37b8cb3003818ed328
Jayandra Hamal <salonhamal278@gmail.com>
2025-02-10 21:06:48 +0545

    Initialize backend with Express

---
+++ instagram-backend/server.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import express from "express";
+
+const app = express();
+
+
+const PORT = process.env.PORT || 5001;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
